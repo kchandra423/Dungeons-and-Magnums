bc6dd1cdded40c167ccbe046d4489ce5e03e6221
KumarSChandra <58375738+KumarSChandra@users.noreply.github.com>
2021-02-18 13:34:47 -0800

    Through intelliJ :)

--- src/kchandra423/entities/obstacles/Obstacle.java
+++ src/kchandra423/entities/obstacles/Obstacle.java
@@ -17,37 +17,5 @@ public class Obstacle extends Entity{
 		return null;
 		
 	}
-//	public collisionDirection intersects(Actor other) {
-//		collisionDirection answer=collisionDirection.NONE;
-//		Shape otherShape=other.getSprite().getShape();
-//		Rectangle myShape= (Rectangle)getSprite().getShape();
-//		try {
-//			if(otherShape.intersects(myShape.getLeft())||otherShape.intersects(myShape.getRight())) {
-//				answer=collisionDirection.X;
-//			}
-//			if(otherShape.intersects(myShape.getTop())||otherShape.intersects(myShape.getBottom())) {
-//				double height=otherShape.getBoundingRectangle().getRight().getLength()/2-10;
-////				if(otherShape.getY()>myShape.getTop().getY()||otherShape.getY()<myShape.getBottom().getY()) {
-//////					
-////
-//////					answer=collisionDirection.Y;
-////				}
-////				else {
-////					answer=collisionDirection.Y;
-//					if(answer==collisionDirection.X) {
-//						
-//					answer=collisionDirection.BOTH;
-//					}
-//					else {
-//						answer=collisionDirection.Y;
-////					}
-//				}
-//			}
-//		} catch (Exception e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		return answer;
-//	}
 	
 }
